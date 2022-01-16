c77c6949304d6f4d43f58893a52fef2a3b8ef382
vineetk13 <vineetkme.99@gmail.com>
2022-01-16 14:16:14 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const cors = require("cors")
 require("./db/striker")
 
 const app = express()
 const strikerRouter = require("./routers/strike")
 
+app.use(cors())
 app.use(express.json())
 app.use(strikerRouter)
 
